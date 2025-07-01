e554867831a5e6d3987bbbb337b454bf93750428
Arthakker <arthuronyeanusi@gmail.com>
2025-07-01 13:43:28 +0100

    Corrected typo issues in schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS nodes{
+CREATE TABLE IF NOT EXISTS nodes(
     id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     longitude DOUBLE NOT NULL,
     latitude DOUBLE NOT NULL
-}
+);
 
-CREATE TABLE IF NOT EXISTS edges{
+CREATE TABLE IF NOT EXISTS edges(
     id INT AUTO_INCREMENT PRIMARY KEY,
     start_node_id INT NOT NULL,
     finish_node_id INT NOT NULL,
@@ -13,6 +13,6 @@ CREATE TABLE IF NOT EXISTS edges{
     is_shade BOOLEAN,
     is_stairs BOOLEAN,
     is_ramp BOOLEAN,
-    FOREIGN KEY (start_node_id) REFRENCES nodes(id) ON DELETE CASCADE,
-    FOREIGN KEY (finish_node_id) REFRENCES nodes(id) ON DELETE CASCADE    
-}
+    FOREIGN KEY (start_node_id) REFERENCES nodes(id) ON DELETE CASCADE,
+    FOREIGN KEY (finish_node_id) REFERENCES nodes(id) ON DELETE CASCADE    
+);

ef802b8f1391e4d4d59ab768c65c365de59f17f6
Arthakker <arthuronyeanusi@gmail.com>
2025-07-01 13:40:27 +0100

    Defined node and edge schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS nodes{
+    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    longitude DOUBLE NOT NULL,
+    latitude DOUBLE NOT NULL
+}
+
+CREATE TABLE IF NOT EXISTS edges{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    start_node_id INT NOT NULL,
+    finish_node_id INT NOT NULL,
+    distance DOUBLE NOT NULL,
+    is_shade BOOLEAN,
+    is_stairs BOOLEAN,
+    is_ramp BOOLEAN,
+    FOREIGN KEY (start_node_id) REFRENCES nodes(id) ON DELETE CASCADE,
+    FOREIGN KEY (finish_node_id) REFRENCES nodes(id) ON DELETE CASCADE    
+}

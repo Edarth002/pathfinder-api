c409bc004ab88ac3a0e13ccb1af38e15639f3997
Arthakker <arthuronyeanusi@gmail.com>
2025-07-03 16:33:24 +0100

    Completed pathRoute

--- routes/pathRoute.js
+++ routes/pathRoute.js
@@ -1,24 +1,39 @@
 import { pathFunction } from "../controllers/pathcontroller.js";
 import { dijkstra, constructPath } from "../utils/dijkstra.js";
 
-export async function pathRoute(req, res){
-    
-    try {
-        const {start, end} = req.query;
+export async function pathRoute(req, res) {
+  try {
+    const { start, end } = req.query;
 
     if (!start || !end) {
-        return res.status(400).send("Missing parameter... Both start and end destinations are required")
+      return res
+        .status(400)
+        .send(
+          "Missing parameter... Both start and end destinations are required"
+        );
     }
-         const graph = await pathFunction()
+    const graph = await pathFunction();
 
-    const {distances, previous} = dijkstra(graph, start)
+    const { distances, previous } = dijkstra(graph, start);
+    const pathNames = constructPath(previous, end);
 
-    const path = constructPath(previous, end)
+    const [nodes] = await db.execute("SELECT * FROM nodes");
 
-    res.json({path, distance:distances[end]})
-    } catch (error) {
-        console.log("Error occured due to: ", error);
-        res.status(500).send("Eror Occured, try refreshing page or enter valid parameters")
-    }
+    //Constructs full path to ensure full details are retuned to frontend to aid map display
+    const fullPath = pathNames.map((name) => {
+      const node = nodes.find((n) => n.name === name);
+      return {
+        name: node.name,
+        latitude: node.latitude,
+        longitude: node.longitude,
+      };
+    });
 
+    res.json({ path: fullPath, distance: distances[end] });
+  } catch (error) {
+    console.log("Error occured due to: ", error);
+    res
+      .status(500)
+      .send("Eror Occured, try refreshing page or enter valid parameters");
+  }
 }
